6b2fe9ec0a7cd2937ae220eb52110a0014069119
Gabriel <shalitmontagne.g@gmail.com>
2022-01-29 13:40:58 -0700

    commit tree bfcaefc48f15b0592d3e607e4c4073376acf0c3b

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.split("")[-1] == "."
   end
 
   def question?
-
+    self.split("")[-1] == "?"
   end
 
   def exclamation?
-
+    self.split("")[-1] == "!"
   end
 
   def count_sentences
-
+    self.split(/\b[?!.]+\s/).length
   end
 end
